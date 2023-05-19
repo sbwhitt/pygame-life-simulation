49f63229e7b0fa3c344b5de44974c09b88252cd3
Samuel Whittenberger <sbw98978@gmail.com>
2023-05-18 23:15:20 -0400

    add clock and middle click spawn

---
+++ src/clock.py
@@ -0,0 +1,12 @@
+import pygame
+import static.settings as settings
+
+
+class Clock:
+    def __init__(self):
+        self.clock = pygame.time.Clock()
+        self.tock = 0
+    
+    def tick(self) -> int:
+        self.clock.tick(settings.CLOCK_RATE)
+        self.tock = self.clock.get_time()
