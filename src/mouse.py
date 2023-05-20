035b8376e8a42f874269c8149e78356340cf961a
Samuel Whittenberger <sbw98978@gmail.com>
2023-05-20 10:43:11 -0400

    init drag cursor

--- src/mouse.py
+++ src/mouse.py
@@ -3,11 +3,18 @@ import src.utils as utils
 import static.colors as colors
 import static.settings as settings
 from src.entity_manager import EntityManager
+from src.window import Window
+
+LEFT_CLICK = 0
+MIDDLE_CLICK = 1
+RIGHT_CLICK = 2
 
 
 class Cursor:
     def __init__(self):
-        pass
+        self.start = (0, 0)
+        self.end = (0, 0)
+        self.rect = None
 
     def get_points(self) -> list[tuple]:
         pos = pygame.mouse.get_pos()
@@ -20,10 +27,11 @@ class Cursor:
 
 
 class Mouse:
-    def __init__(self):
+    def __init__(self, window: Window):
+        self.window = window
         self.cursor = Cursor()
-        self.drag_start = (0, 0)
-        self.dragging = False
+        self.drag_start = [(0, 0), (0, 0), (0, 0)]
+        self.dragging = [False, False, False]
         self.click_timer = 0
         self.spawn_modulus = 250
         self.spawn_interval = 0
@@ -45,22 +53,47 @@ class Mouse:
         self.spawn_interval = False
 
     def highlight_cursor(self, screen: pygame.display) -> None:
-        pygame.draw.lines(screen, colors.BLACK, True, points=self.cursor.get_points())
+        if self.dragging[LEFT_CLICK]:
+            pygame.draw.rect(screen, colors.BLACK, self.cursor.rect)
+        else:
+            pygame.draw.lines(screen, colors.BLACK, True, points=self.cursor.get_points())
+    
+    def drag(self, button: int) -> None:
+        if not self.dragging[button]:
+            self._start_drag(button)
+        elif button == LEFT_CLICK:
+            self.cursor.end = pygame.mouse.get_pos()
+            self._build_cursor_rect(self.window.offset)
+    
+    def stop_drag(self, button: int) -> None:
+        self.dragging[button] = False
 
-    def get_drag_dir(self) -> tuple:
+    def get_drag_dir(self, button: int) -> tuple:
         pos = pygame.mouse.get_pos()
         dirx = 0
         diry = 0
-        if pos[0] > self.drag_start[0]:
+        start = self.drag_start[button]
+        if pos[0] > start[0]:
             dirx = settings.ENT_WIDTH*2
-        elif pos[0] < self.drag_start[0]:
+        elif pos[0] < start[0]:
             dirx = -settings.ENT_WIDTH*2
-        if pos[1] > self.drag_start[1]:
+        if pos[1] > start[1]:
             diry = settings.ENT_WIDTH*2
-        elif pos[1] < self.drag_start[1]:
+        elif pos[1] < start[1]:
             diry = -settings.ENT_WIDTH*2
-
         return (dirx, diry)
+
+    # helpers
+    def _start_drag(self, button: int) -> None:
+        self.dragging[button] = True
+        m_pos = pygame.mouse.get_pos()
+        self.drag_start[button] = m_pos
+        if button == LEFT_CLICK:
+            self.cursor.start = m_pos
+            self.drag(LEFT_CLICK)
+    
+    def _build_cursor_rect(self, offset: tuple) -> None:
+        self.cursor.rect = utils.get_rect_from_twoples(self.cursor.start, self.cursor.end)
 
     def _increase_spawn_interval(self, elapsed) -> bool:
         self.click_timer += elapsed
--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,56 @@
 import pygame
 import random
+import static.settings as settings
 
-def add_twoples(c1: tuple, c2: tuple) -> tuple:
+
+def add_twoples(t1: tuple, t2: tuple) -> tuple:
     '''Returns sum of two element tuples'''
-    return (c1[0] + c2[0], c1[1] + c2[1]) if len(c1) == len(c2) == 2 else None
+    return (t1[0] + t2[0], t1[1] + t2[1]) if len(t1) == len(t2) == 2 else None
 
-def multiply_twople_by_constant(coord: tuple, const: int) -> tuple:
+def subtract_twoples(t1: tuple, t2: tuple) -> tuple:
+    '''Returns sum of two element tuples'''
+    return (t1[0] - t2[0], t1[1] - t2[1]) if len(t1) == len(t2) == 2 else None
+
+def multiply_twople_by_constant(t1: tuple, const: int) -> tuple:
     '''Returns two element tuple with all elements multiplied by given constant'''
-    return (coord[0] * const, coord[1] * const) if len(coord) == 2 else None
+    return (t1[0] * const, t1[1] * const) if len(t1) == 2 else None
 
-def get_random_color() -> pygame.Color:
-    '''Returns random pygame color between values 10, 245'''
-    return pygame.Color(
-        random.randint(10, 245),
-        random.randint(10, 245),
-        random.randint(10, 245)
-    )
+def get_random_color(start: int=-1, end: int=-1) -> pygame.Color:
+    '''Returns random pygame color between given values or 10, 245 by default'''
+    if (start > 0 and end > 0) and (start <= 255 and end <= 255):
+        return pygame.Color(
+            random.randint(start, end),
+            random.randint(start, end),
+            random.randint(start, end)
+        )
+    else:
+        return pygame.Color(
+            random.randint(10, 245),
+            random.randint(10, 245),
+            random.randint(10, 245)
+        )
+
+def get_tile_pos(t1: tuple, offset: tuple=None, top_left: bool=False, top_right: bool=False, bottom_left: bool=False, bottom_right: bool=False) -> tuple:
+    '''
+    Returns the tile start coordinates for the position given plus an optional offset.
+    Returns top left point by default
+    '''
+    loc = add_twoples(t1, offset) if offset else t1
+    if top_left:
+        return (loc[0]-(loc[0] % settings.ENT_WIDTH), loc[1]-(loc[1] % settings.ENT_WIDTH))
+    if top_right:
+        return (loc[0]+(loc[0] % settings.ENT_WIDTH), loc[1]-(loc[1] % settings.ENT_WIDTH))
+    if bottom_left:
+        return (loc[0]-(loc[0] % settings.ENT_WIDTH), loc[1]+(loc[1] % settings.ENT_WIDTH))
+    if bottom_right:
+        return (loc[0]+(loc[0] % settings.ENT_WIDTH), loc[1]+(loc[1] % settings.ENT_WIDTH))
+    return (loc[0]-(loc[0] % settings.ENT_WIDTH), loc[1]-(loc[1] % settings.ENT_WIDTH))
+
+def get_rect_from_twoples(t1: tuple, t2: tuple, offset: tuple=None) -> pygame.rect.Rect:
+    '''Returns a pygame rect with top left and bottom right corners t1 and/or t2'''
+    # [t1, t2, (t1[0], t2[1]), (t2[0], t1[1])]
+    left = t1[0] if t1[0] < t2[0] else t2[0]
+    top = t1[1] if t1[1] < t2[1] else t2[1]
+    width = abs(t1[0] - t2[0])
+    height = abs(t1[1] - t2[1])
+    return pygame.rect.Rect(left, top, width, height)
