730d47a6498c64fbc519f3b3d9065470be0ac302
Samuel Whittenberger <sbw98978@gmail.com>
2023-05-19 23:14:41 -0400

    use utils

--- src/window.py
+++ src/window.py
@@ -1,3 +1,4 @@
+import src.utils as utils
 import static.settings as settings
 
 
@@ -9,9 +10,9 @@ class Window:
     
     def move(self, dir: tuple) -> None:
         if self.offset[0] + dir[0] >= 0 and self.offset[0] + self.width + dir[0] <= settings.WORLD_SIZE:
-            self.offset = (self.offset[0] + dir[0], self.offset[1])
+            self.offset = utils.add_twoples(self.offset, (dir[0], 0))
         if self.offset[1] + dir[1] >= 0 and self.offset[1] + self.height + dir[1] <= settings.WORLD_SIZE:
-            self.offset = (self.offset[0], self.offset[1] + dir[1])
+            self.offset = utils.add_twoples(self.offset, (0, dir[1]))
     
     def contains(self, point: tuple) -> bool:
         return (point[0] >= self.offset[0] and 
