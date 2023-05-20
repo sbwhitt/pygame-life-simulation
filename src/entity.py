d105a94f72b9aaa1be7f88fca753c9a2de61f673
Samuel Whittenberger <sbw98978@gmail.com>
2023-05-19 23:07:43 -0400

    revert disease rate change

--- src/entity.py
+++ src/entity.py
@@ -33,7 +33,7 @@ class Entity:
         spawn_loc = self._choose_spawn_location()
         if (self.dna.nourished or r == 1) and not self.dna.diseased and spawn_loc:
             offspring = Entity(spawn_loc[0], spawn_loc[1])
-            if random.randint(1, 10) == 1:
+            if random.randint(1, 400) == 1:
                 # chance to mutate diseased into a new random color
                 offspring.dna.mutate()
             else:
